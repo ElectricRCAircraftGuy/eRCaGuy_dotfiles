e6a7418f5f00953b117a8931d5232bc816efc6ad
Gabriel Staples <ercaguy@gmail.com>
2020-02-02 20:02:01 -0800

    Bug fix: update apt-cacher scripts to find absolute path to main script so they always work

--- useful_scripts/apt-cacher-server_proxy_status.sh
+++ useful_scripts/apt-cacher-server_proxy_status.sh
@@ -17,5 +17,15 @@
 # References
 # 1. https://help.ubuntu.com/community/Apt-Cacher-Server
 
-./apt-cacher-server_proxy.sh status
+# First, find the directory where this script lies
+# - See: https://stackoverflow.com/questions/59895/how-to-get-the-source-directory-of-a-bash-script-from-within-the-script-itself/246128#246128
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+
+${DIR}/apt-cacher-server_proxy.sh status
 
