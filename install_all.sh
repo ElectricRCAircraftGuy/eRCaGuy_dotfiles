222bbda8395e96ca74ba6c558cc0c3a2688473f8
Gabriel Staples <ercaguy@gmail.com>
2020-03-17 00:06:40 -0700

    Add Arduino & other install script portions to install_all.sh script

--- install_all.sh
+++ install_all.sh
@@ -30,11 +30,68 @@ echo -e "Beginning installation. Note that if it asks if you'd like to \"overwri
 "a file, simply pressing Enter will default to \"No\", which is the safe option to take."
 echo    "-----------------------------------------------------------------------------------------"
 
+
+# arduino
+# See "arduino/readme--arduino.md"
+echo "= Arduino stuff ="
+echo "Adding user to \"dialout\" group."
+echo "Please log out and log back in for this to take effect."
+sudo usermod -a -G dialout $USERNAME
+echo "Also, unplug and plug back in any USBasp programmer, if applicable."
+sudo cp -i etc/udev/rules.d/99-USBasp.rules /etc/udev/rules.d
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
 # Desktop_launchers
+echo "= Desktop_launchers stuff ="
 echo "Copying \"Desktop_launchers\" files to ~/Desktop_launchers"
 cp -ri Desktop_launchers/*.desktop ~/Desktop_launchers
 cp -ri Desktop_launchers/*.md ~/Desktop_launchers
 echo "Installing \"desktop_file_install\" & \"desktop_file_uninstall\" scripts"
 ln -si "$PWD/Desktop_launchers/desktop_file_install.sh" ~/bin/${PREPEND_STR}desktop_file_install
 ln -si "$PWD/Desktop_launchers/desktop_file_uninstall.sh" ~/bin/${PREPEND_STR}desktop_file_uninstall
+
+# eclipse
+# Do it manually
+# Esp. see "eclipse/Eclipse setup instructions on a new Linux (or other OS) computer.pdf"
+
+# etc
+# Arduino USBasp stuff already done above.
+# See also "etc/udev/rules.d/readme--udev_rules.md"
+
+# NoMachine
+# Do it manually
+# See: "NoMachine/readme--NoMachine.md"
+
+# rsync
+# For sample usage, see my answer here: 
+# https://superuser.com/questions/1271882/convert-ntfs-partition-to-ext4-how-to-copy-the-data/1464264#1464264
+# and here: https://unix.stackexchange.com/questions/65077/is-it-possible-to-see-cp-speed-and-percent-copied/567828#567828
+
+# segger programmer
+# Do it manually
+# See my answer here: https://stackoverflow.com/questions/57307738/is-there-anybody-using-keil-mdk-on-linux-through-wine/57313990#57313990
+
+# Templates
+echo "= Templates stuff ="
+echo "Copying \"Templates\" files to ~/Templates"
+cp -ri Templates ~
+
+# tmux
+# TODO
+
+# useful_scripts
+# TODO
+
+# Sublime Text 3, incl. as a git editor, & copying main settings over
+# TODO
+
+# ~/.bashrc & ~/.profile
+# TODO
+
+# .gitconfig
+# TODO
+
+
+
 
