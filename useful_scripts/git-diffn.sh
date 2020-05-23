b99bc84a6ff4ca8ddbe25777e28f5dd6f460fcd4
Gabriel Staples <ercaguy@gmail.com>
2020-05-22 23:25:22 -0700

    git-diffn.sh: expand the gawk function with whitespace and indenting
    I simply followed the lead that @Ed Morton placed into my question here:
    https://stackoverflow.com/questions/61932427/git-diff-with-line-numbers-and-proper-code-alignment-indentation

--- useful_scripts/git-diffn.sh
+++ useful_scripts/git-diffn.sh
@@ -82,14 +82,43 @@
 
 
 git diff --color=always "$@" | \
-gawk '{bare=$0;gsub("\033[[][0-9]*m","",bare)};
-  match(bare,"^@@ -([0-9]+),[0-9]+ [+]([0-9]+),[0-9]+ @@",a){left=a[1];right=a[2];print;next};
-  bare ~ /^(---|\+\+\+|[^-+ ])/{print;next};
-  {line=gensub("^(\033[[][0-9]*m)?(.)","\\2\\1",1,$0)};
-  bare~/^-/{printf   "\033[31m-%+4s     \033[m:\033[31m%s\n", left++, line;next};
-  bare~/^[+]/{printf "\033[32m+     %+4s\033[m:\033[32m%s\n", right++, line;next};
-  {printf                    " %+4s,%+4s:%s\n", left++, right++, line;next}' | \
-  less -R -F -X
+gawk '
+{
+    bare = $0
+    gsub("\033[[][0-9]*m", "", bare)
+}
+
+match(bare, "^@@ -([0-9]+),[0-9]+ [+]([0-9]+),[0-9]+ @@", a) {
+    left = a[1]
+    right = a[2]
+    print
+    next
+}
+
+bare ~ /^(---|\+\+\+|[^-+ ])/ {
+    print
+    next
+}
+
+{
+    line = gensub("^(\033[[][0-9]*m)?(.)", "\\2\\1", 1, $0)
+}
+
+bare ~ /^-/ {
+    printf "\033[31m-%+4s     \033[m:\033[31m%s\n", left++, line
+    next
+}
+
+bare ~ /^[+]/ {
+    printf "\033[32m+     %+4s\033[m:\033[32m%s\n", right++, line
+    next
+}
+
+{
+    printf " %+4s,%+4s:%s\n", left++, right++, line
+    next
+}' | less -R -F -X
+
 
 # ^^  use -R to interpret ANSI color codes, -F to quit if less than one-screen, and -X to not clear
 # the screen when less exits!
