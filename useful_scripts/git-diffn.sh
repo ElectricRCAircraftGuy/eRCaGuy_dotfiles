31757a70c01c624b21f4b151d0afa98980a64aca
Gabriel Staples <ercaguy@gmail.com>
2020-05-22 22:52:02 -0700

    Add header to git-diffn.sh

--- useful_scripts/git-diffn.sh
+++ useful_scripts/git-diffn.sh
@@ -1,8 +1,45 @@
 #!/bin/bash
 
-# Install:
-# ln -si "${PWD}/git-diffn.sh" ~/bin/git-diffn
-# ln -si "${PWD}/git-diffn.sh" ~/bin/gs_git-diffn
+# This file is part of eRCaGuy_dotfiles: https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
+
+# Author: Gabriel Staples
+# Status: Work in Progress (WIP)
+
+# git-diffn.sh
+# - a drop-in replacement for `git diff` which also shows line 'n'umbers! Use it *exactly* like
+#   `git diff`, except you'll see these beautiful line numbers as well to help you make sense of
+#   your changes. 
+# - since it's just a wrapper around `git diff`, it accepts ALL options and parameters that 
+#   `git diff` accepts. Examples:
+#   - `git diffn HEAD~`
+#   - `git diffn HEAD~3..HEAD~2`
+# - the one caveat and difference is that if you want to disable the output color, you must use
+#   `--no-color` or `--color=never` as your FIRST ARGUMENT, as I'm parsing it as a positional 
+#   argument. Examples: 
+#   - `git diffn --color=never HEAD~`
+#   - `git diffn --no-color HEAD~3..HEAD~2`
+
+# INSTALLATION INSTRUCTIONS:
+# 1. Create a symlink in ~/bin to this script so you can run it from anywhere as `git diffn` OR
+#    as `git-diffn` OR as `gs_git-diffn` Note that "gs" is my initials. I do this last version 
+#    so I can find all scripts I've written really easily by simply typing "gs_" + Tab + Tab. 
+#       cd /path/to/here
+#       mkdir -p ~/bin
+#       ln -si "${PWD}/git-diffn.sh" ~/bin/git-diffn
+#       ln -si "${PWD}/git-diffn.sh" ~/bin/gs_git-diffn
+# 2. Now you can use this command directly anywhere you like in any of the 3 ways:
+#   1. `git diffn`
+#   2. `git-diffn`
+#   3. `gs_git-diffn`
+
+# References:
+# 1. This script heavily borrows from @PFudd's script here:
+#    https://stackoverflow.com/questions/24455377/git-diff-with-line-numbers-git-log-with-line-numbers/33249416#33249416
+# 2. @PFudd expands on @Andy Talkowski's code from here:
+#    https://stackoverflow.com/questions/24455377/git-diff-with-line-numbers-git-log-with-line-numbers/32616440#32616440
+# 3. I also received help from @Ed Morton and @Inian here: 
+#    https://stackoverflow.com/questions/61932427/git-diff-with-line-numbers-and-proper-code-alignment-indentation
+
 
 ####### ADD A MECHANISM of turning off color, in case the user wants no color. ie: 
 # if --color=never then don't do --color=always
