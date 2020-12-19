cc6dc89a9d284b4a245fc147d1e7829e0fe209ea
Gabriel Staples <ercaguy@gmail.com>
2020-12-19 15:24:22 -0800

    ros_readbagfile.py: add yaml comment markers `#` in front of non-yaml output

--- useful_scripts/ros_readbagfile.py
+++ useful_scripts/ros_readbagfile.py
@@ -47,6 +47,7 @@ import rosbag
 
 import os
 import sys
+import textwrap
 
 # `topics=None` means to read ALL topics; see:
 # https://docs.ros.org/api/rosbag/html/python/rosbag.bag.Bag-class.html#read_messages
@@ -55,27 +56,27 @@ READ_ALL_TOPICS = None
 
 def printHelp():
     cmd_name = os.path.basename(sys.argv[0])
-    help = """
-Usage: {} <mybagfile.bag> [topic1] [topic2] [topic3] [...topicN]
+    help = textwrap.dedent("""
+        Usage: {} <mybagfile.bag> [topic1] [topic2] [topic3] [...topicN]
 
-Reads and prints all messages published on the specified topics from the specified ROS bag file. If
-no topics are specified, it will print ALL messages published on ALL topics found in the bag file.
-For large bag files, on the order of 10 to 20 GB or so, expect the script to take on the order of
-1 to 4 minutes or so assuming you have a fast Solid-State Drive (SSD).
+        Reads and prints all messages published on the specified topics from the specified ROS bag file. If
+        no topics are specified, it will print ALL messages published on ALL topics found in the bag file.
+        For large bag files, on the order of 10 to 20 GB or so, expect the script to take on the order of
+        1 to 4 minutes or so assuming you have a fast Solid-State Drive (SSD).
 
-TODO: converting this script from Python to C++ could potentially speed this up by an estimated
-factor of 13x or so, decreasing the processing time from a couple minutes to perhaps a dozen
-seconds.
+        TODO: converting this script from Python to C++ could potentially speed this up by an estimated
+        factor of 13x or so, decreasing the processing time from a couple minutes to perhaps a dozen
+        seconds.
 
-Examples:
-1. Print all messages published to the "/speed", "/vel", and "/dist" topics and stored in the
-"mybagfile.bag" ROS bag file to the screen:
-    {} mybagfile.bag /speed /vel /dist
-2. Same as above, except also stores the printed output into an output file, "output.txt" as well:
-    {} mybagfile.bag /speed /vel /dist | tee output.txt
-3. [MY FAVORITE] Same as above, except also times how long the whole process takes:
-    time {} mybagfile.bag /speed /vel /dist | tee output.txt
-""".format(cmd_name, cmd_name, cmd_name, cmd_name)
+        Examples:
+        1. Print all messages published to the "/speed", "/vel", and "/dist" topics and stored in the
+        "mybagfile.bag" ROS bag file to the screen:
+            {} mybagfile.bag /speed /vel /dist
+        2. Same as above, except also stores the printed output into an output file, "output.txt" as well:
+            {} mybagfile.bag /speed /vel /dist | tee output.txt
+        3. [MY FAVORITE] Same as above, except also times how long the whole process takes:
+            time {} mybagfile.bag /speed /vel /dist | tee output.txt
+        """).format(cmd_name, cmd_name, cmd_name, cmd_name)
 
     print(help)
 
@@ -97,7 +98,7 @@ def parseArgs():
     args = Args()
 
     if len(sys.argv) == 1:
-        print("ERROR: no input args; 1 minimum required.")
+        print("# ERROR: no input args; 1 minimum required.")
         printHelp()
         sys.exit()
 
@@ -111,16 +112,17 @@ def parseArgs():
     if len(sys.argv) >= 3:
         args.topics_to_read = sys.argv[2:]
 
-    print("Scanning ROS bag file \"{}\"".format(args.bag_file_in))
-    print("for the following topics:")
+    print("# Scanning ROS bag file \"{}\"".format(args.bag_file_in))
+    print("# for the following topics:")
     if not args.topics_to_read:
         # list is empty
-        print("  ALL TOPICS in the bag file")
+        print("#    ALL TOPICS in the bag file")
     else:
         for topic in args.topics_to_read:
-            print("  {}".format(topic))
+            print("#    {}".format(topic))
 
-    print("=======================================")
+    print("# =======================================")
+    print("# =======================================")
     return args
 
 
@@ -140,26 +142,27 @@ def printMsgsInBagFile(args):
             msg_counters[topic] += 1;
 
         # Print topic name and message receipt info
-        print("topic:           " + topic)
-        print("msg #:           %u" % msg_counters[topic])
+        print("# topic:           " + topic)
+        print("# msg_count:       %u" % msg_counters[topic])
         # the comma at the end prevents the newline char being printed at the end in Python2; see:
         # https://www.geeksforgeeks.org/print-without-newline-python/
-        print("timestamp (sec):"),
-        print("%.9f" % t.to_sec())
-        print("- - -")
+        print("# timestamp (sec): {:.9f}".format(t.to_sec())),
+        print("# - - -")
 
         # Print the message
         print(msg)
-        print("=======================================")
+        print("# =======================================")
+
+    print("# =======================================")
 
     if no_msgs_found:
-        print("NO MESSAGES FOUND IN THESE TOPICS")
+        print("# NO MESSAGES FOUND IN THESE TOPICS")
 
-    print("Total messages found = {}.".format(total_count))
+    print("# Total messages found = {}.".format(total_count))
     ##### TODO: PRINT MESSAGE COUNT FOR EACH MSG TYPE TOO! ie: just print all key/value pairs in the
     # `msg_counters` dict. What order to print them in? Probably just alphabetical.
 
-    print("DONE.")
+    print("# DONE.")
 
 
 # If this file is called directly, as opposed to imported, run this:
