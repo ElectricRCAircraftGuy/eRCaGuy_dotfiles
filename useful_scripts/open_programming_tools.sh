13aeea61372c6ac09f7aaee6de5a61e66aa90f87
Gabriel Staples <ercaguy@gmail.com>
2021-01-23 21:42:00 -0800

    Update useful_scripts/open_programming_tools.sh to work w/terminator OR gnome-terminal!

--- useful_scripts/open_programming_tools.sh
+++ useful_scripts/open_programming_tools.sh
@@ -1,67 +1,73 @@
 #!/bin/bash
 
-# Author: Gabriel Staples
-
 # This file is part of eRCaGuy_dotfiles: https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
 
-# INSTRUCTIONS:
-# 1. Note: this script opens up several tabs in a terminal, each tab with its own title and command, then it opens up
-#    a bunch of programs I use daily for development work. See the Reference URL below for additional info.
-# 2. Edit the titles and commands below. Ensure it opens up the programs you want, and in the right order, 
-#    then to install, either:
-# A) copy it to your Desktop: `cp -i open_programming_tools.sh ~/Desktop` OR 
-# B) create a symbolic link to it on your Desktop:
-#		cd /path/to/here
-#		ln -s "${PWD}/open_programming_tools.sh" ~/Desktop/open_programming_tools
-# OR
-# C) (My Preference) Create a .desktop file on your Desktop which calls this script. Use the .desktop file
-#    provided in "eRCaGuy_dotfiles/Desktop_launchers/open_programming_tools.desktop" for this purpose.
-
 # open_programming_tools.sh
-# - a script to open up a terminal in the right location with multiple tabs, etc, so I can begin working
-#   & programming quickly!
+# - a script to open up a terminal in the right location with multiple tabs, etc, so I can begin
+#   working & programming quickly!
 # - also have it open up other programs & things as necessary
 
 # Gabriel Staples
-# First Written: 19 Apr. 2018 
-# Updated: 2020 & later--now git version-controlled in the above dotfiles project
+# First Written: 19 Apr. 2018
 
-# References & Additional Reading:
-# - My own answer!: 
-#   https://askubuntu.com/questions/315408/open-terminal-with-multiple-tabs-and-execute-application/1026563#1026563
+# PURPOSE & USAGE:
+# This script opens up several tabs in a terminal--each tab with its own title and command, then it
+# opens up a bunch of programs I use for daily development work. I run this script once after each
+# reboot by simply clicking on the corresponding desktop file shortcut on my desktop in order to get
+# my computer ready-to-go again.
+# See my answer here for additional info:
+# https://askubuntu.com/questions/315408/open-terminal-with-multiple-tabs-and-execute-application/1026563#1026563
 
-# TO VIEW DESKTOP LAUNCHERS TO SEE WHAT THE COMMANDS ARE FOR OPENING CERTAIN PROGRAMS:    
-# `ls /usr/share/applications`, then:
-# `cat /usr/share/applications/launcher_of_interest.desktop`
+# INSTALLATION INSTRUCTIONS:
+#
+# 1. Copy this file to "~/bin" to 1) add it to your PATH, and 2) give you a copy to customize and
+# edit.
+#       cd /path/to/here
+#       mkdir -p ~/bin
+#       cp -i open_programming_tools.sh ~/bin/gs_open_programming_tools
+#
+# 2. Edit the list of programs to open below. Ensure each program you want opened is called, and in
+# the order you want. TO VIEW DESKTOP LAUNCHERS TO SEE WHAT THE COMMANDS ARE FOR OPENING CERTAIN
+# PROGRAMS you already have installed, you can consult the various *.desktop files in your
+# "/usr/share/applications" directory:
+#       ls /usr/share/applications
+#       # then
+#       cat /usr/share/applications/launcher_of_interest.desktop
+#
+# 3. Add a symlink to the corresponding desktop file to your Desktop so you can run this script
+# by double-clicking on the icon on your Desktop:
+#       cd /path/to/eRCaGuy_dotfiles/Desktop_launchers
+#       ln -si "${PWD}/open_programming_tools.desktop" ~/Desktop
+#
+# 4. Now double-click on the "Open Programming Tools" icon on your desktop to run this script!
+# DONE!
 
-
-# **Use ampersands (&) after each command below to allow them to all open up in parallel to speed up 
+# **Use ampersands (&) after each command below to allow them to all open up in parallel to speed up
 # the opening process!**
 
-# 1) Open up a terminal with multiple tabs
-# - Set all tab titles & cmds in ~/.bashrc
+# 1) Open up a terminal with multiple tabs. For this to work, this assumes you've already installed
+# the necessary `~/.bash_aliases`, and `~/.bash_aliases_private` files from this directory. Set
+# all your tabs to open in `~/.bash_aliases_private`??? <==========
+# See this readme for details:
+# https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles/tree/master/home#1-installation
 
-# Export this variable so your ~/.bashrc file will see it and do the magic.
-export OPEN_DEFAULT_TABS=true
-# Open a new terminal window, which by default also sources your ~/.bashrc file again, 
-# thereby kicking off the process since you set the `OPEN_DEFAULT_TABS` variable just above.
+# inter-process communication is done here via the existence of the below temporary file!
+mkdir -p ~/temp
+if [ ! -f ~/temp/.open_default_tabs ]; then  # for meaning of `-f`, see `man test`
+    touch ~/temp/.open_default_tabs
+fi
+
+# Choose your terminal application. Only ONE of these should be uncommented! I prefer `terminator`
+# (install it with `sudo apt install terminator`), but the default terminal application that Ubuntu
+# comes with is `gnome-terminal`.
+terminator&
 # gnome-terminal&
-terminator& ### TODO: MAKE A WAY TO EASILY CHOOSE WHICH TERMINAL YOU WANT, BY SETTING A VARIABLE
-            # INSIDE ~/.bashrc or something. gnome-terminal or terminator
-# Now reset and unexport the above variable so it will no longer be in force; note that it's possible only 
-# one of the below lines is *required*, but let's do both for good measure
-# - This solves the bug where right-clicking in the nemo file manager (which is opened below) and 
-#   going to "Open in Terminal" opens up a new terminal with all the default tabs (bug!), rather 
-#   than a terminal with only a new tab in the currently-opened directory.
-sleep 1 ######## necessary????
-OPEN_DEFAULT_TABS=      # set this variable back to an empty string so it's no longer in force
-unset OPEN_DEFAULT_TABS # unexport it
 
-# 2) Open up other programs:
+# 2) Open up all other programs you want:
 
-nemo "$HOME/GS/dev"& # Nemo file manager; start in this folder
-subl& # Sublime Text editor
-# gnome-system-monitor& # nah, add to Ubuntu's system startup menu to open this at boot instead
+nemo "$HOME/GS/dev"&  # Nemo file manager; start in this folder
+subl&                 # Sublime Text editor
+# gnome-system-monitor&  # nah, added to Ubuntu's system startup menu to open this at boot instead
 libreoffice --writer&
 google-chrome&
 $HOME/eclipse/cpp-2019-12/eclipse/eclipse&
