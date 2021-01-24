947b1322c4be880dd6485ec89a20be2d9929187f
Gabriel Staples <ercaguy@gmail.com>
2021-01-23 21:02:43 -0800

    Update a few desktop files & scripts
    	modified:   Desktop_launchers/open_programming_tools.desktop
    	new file:   Desktop_launchers/touchpad_toggle.desktop
    	modified:   useful_scripts/touchpad_toggle.sh

--- useful_scripts/touchpad_toggle.sh
+++ useful_scripts/touchpad_toggle.sh
@@ -15,39 +15,40 @@
 #		mkdir -p ~/bin
 # 2. Copy this script to ~/bin with a name that you like. Ex:
 #		cp -i touchpad_toggle.sh ~/bin/gs_touchpad_toggle
-# 3. Adjust the USER INPUTS section below, as required. This will require running `xinput` once from
-#    the command line to ensure that TouchPad/Touchpad is spelled right, for instance. See below for details.
+# 3. In the newly-copied file in ~/bin, adjust the USER INPUTS section below, as required. This will
+#    require running `xinput` once from the command line to ensure that TouchPad/Touchpad is
+#    spelled right, for instance. See below for details.
 # 4. Create a keyboard shortcut to associate Ctrl + Alt + P with this script via (on Ubuntu 18.04)
 #    Settings --> Devices --> Keyboard --> scroll to very bottom and click the "+" button to add a custom
 #    shortcut, then name it "Touchpad Toggle", give it the command "gs_touchpad_toggle", and associate it
 #    with the Ctrl + Alt + P keyboard shortcut. Test this shortcut now to ensure it works!
-# 5. Add a startup call to toggle touchpad OFF with every boot: press Windows (Super) key --> search for 
+# 5. Add a startup call to toggle touchpad OFF with every boot: press Windows (Super) key --> search for
 #    "Startup Applications", and open it --> click "Add" to create a new entry --> Name it
-#    "disable touchpad (Ctrl + Alt + P)", enter "gs_touchpad_toggle --off" for the "Command", and 
+#    "disable touchpad (Ctrl + Alt + P)", enter "gs_touchpad_toggle --off" for the "Command", and
 #    "found in ~/bin" for the "Comment." Click "Save", then "Close".
 # 6. Done! Your Touchpad and Touchscreen will automatically become DISABLED at every boot! To toggle it on/off
 #    use the Ctrl + Alt + P shortcut you set up. This is very useful to quickly swap between using an external
 #    mouse vs the built-in touchpad or touchscreen.
 
 # Author: Gabriel Staples
-# Started: 2 Apr. 2018 
-# Update History (newest on TOP): 
+# Started: 2 Apr. 2018
+# Update History (newest on TOP):
 #   every date thereafter - refer to the eRCaGuy_dotfiles project referenced above; see git commits
-#   28 Jan. 2020 - added in lines to disable Touchscreen too, as well as show ID numbers of 
+#   28 Jan. 2020 - added in lines to disable Touchscreen too, as well as show ID numbers of
 #                  Touchscreen & Touchpad
 #   22 June 2019 - added in the imwheel stuff to not mess up track pad scrolling when
 #                  track pad is in use
 
 # References (in order of progression):
 # 1. negusp described xinput: https://askubuntu.com/questions/844151/enable-disable-touchpad/844218#844218
-# 2. Almas Dusal does some fancy sed stuff & turns negusp's answer into a script: 
+# 2. Almas Dusal does some fancy sed stuff & turns negusp's answer into a script:
 #    https://askubuntu.com/questions/844151/enable-disable-touchpad/874865#874865
 # 3. I turn it into a beter script, attach it to a Ctrl + Alt + P shortcut, & do a zenity GUI popup window as well:
 #    https://askubuntu.com/questions/844151/enable-disable-touchpad/1109515#1109515
 # 4. I add imwheel to my script to also fix Chrome mouse scroll wheel speed problem at the same time:
 #    https://askubuntu.com/questions/254367/permanently-fix-chrome-scroll-speed/991680#991680
-# 5. I put this script on Github, and posted a snapshot of it on this answer here: 
-#    https://askubuntu.com/questions/198572/how-do-i-disable-the-touchscreen-drivers/1206493#1206493 
+# 5. I put this script on Github, and posted a snapshot of it on this answer here:
+#    https://askubuntu.com/questions/198572/how-do-i-disable-the-touchscreen-drivers/1206493#1206493
 # 6. I put this script into my eRCaGuy_dotfiles, and continued to improve upon it here:
 #	 https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
 
@@ -55,7 +56,7 @@
 # USER INPUTS--ADJUST THESE!:
 
 # `xinput` search strings for these devices
-# - Manually run `xinput` on your PC, look at the output, and adjust these search strings as necessary for your 
+# - Manually run `xinput` on your PC, look at the output, and adjust these search strings as necessary for your
 #   particular hardware and machine!
 # TOUCHPAD_STR="TouchPad" # some computers need it spelled like this with a capital 'P' in TouchPad
 TOUCHPAD_STR="Touchpad" # other computers need it spelled like this with a lower-case 'p' in Touchpad
@@ -111,7 +112,7 @@ if [ "$newstate" = "OFF" ];then
     zenity --info --text "${PRINT_TEXT} DISABLED" --timeout=2
 elif [ "$newstate" = "ON" ];then
 	# Turn touchpad & touchscreen ON, & turn imwheel OFF so it doesn't interfere w/trackpad scrolling, since user must
-	# be using the touchpad and/or touchscreen 
+	# be using the touchpad and/or touchscreen
 	echo "Turning TouchpadId $TouchpadId & TouchscreenId $TouchscreenId ON."
     killall imwheel # turn OFF imwheel to keep imwheel from interfering with proper track pad scrolling
     xinput --enable "$TouchpadId"
