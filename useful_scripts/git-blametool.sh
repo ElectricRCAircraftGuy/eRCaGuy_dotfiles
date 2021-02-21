71007c4ec48ec63d12b1ee9c38fe1bbad5801696
Gabriel Staples <ercaguy@gmail.com>
2021-02-20 23:43:21 -0800

    git-blametool.sh: improve argument parsing to be more robust
    ...and to use more-advanced techniques. See here:
    https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash/14203146#14203146

--- useful_scripts/git-blametool.sh
+++ useful_scripts/git-blametool.sh
@@ -53,7 +53,7 @@ RETURN_CODE_SUCCESS=0
 RETURN_CODE_ERROR=1
 
 SCRIPT_NAME="$(basename "$0")"
-VERSION_SHORT_STR="git blametool (run as '$SCRIPT_NAME') version $VERSION"
+VERSION_SHORT_STR="git blametool ('$SCRIPT_NAME') version $VERSION"
 VERSION_LONG_STR="\
 $VERSION_SHORT_STR
 Author = $AUTHOR
@@ -77,7 +77,7 @@ Usage:
     around 'git blame', it takes ALL of the same parameters that 'git blame' takes too! A typical
     usage of 'git blametool' is like this:
 
-    $SCRIPT_NAME [commit_hash] <file_path>
+    $SCRIPT_NAME [-h|-?] [-v|--version] [commit_hash] <file_path>
             Open up the 'git blame' output for file \"file_path\" from commit or branch
             \"commit_hash\" in your editor specified by 'git config blametool.editor'.
             If no editor is set, the default, '$DEFAULT_EDITOR' ($DEFAULT_EDITOR_DESCRIPTION) is
@@ -87,17 +87,18 @@ Usage:
                     git config --global blametool.editor gedit
     $SCRIPT_NAME
             print this help menu
-    $SCRIPT_NAME -h
+    $SCRIPT_NAME [-h|-?]
             print this help menu
-    $SCRIPT_NAME -?
-            print this help menu
-    $SCRIPT_NAME -v
+    $SCRIPT_NAME [-v|--version]
             print author & version
 
 Examples:
 
     $SCRIPT_NAME master path/to/myfile.c
     $SCRIPT_NAME path/to/myfile.c
+    $SCRIPT_NAME -h
+    $SCRIPT_NAME -v
+    $SCRIPT_NAME --version
 
 Configuration:
 
@@ -136,37 +137,22 @@ print_version() {
     echo "$VERSION_LONG_STR"
 }
 
-# Parse the 'git blametool'-specific arguments first
-parse_args() {
-    if [ $# -eq 0 ]; then
-        echo "No arguments supplied"
-        print_help
-        exit $RETURN_CODE_ERROR
-    fi
-
-    # Help menu
-    if [ "$1" == "-h" ] || [ "$1" == "-?" ]; then
-        print_help
-        exit $RETURN_CODE_SUCCESS
-    fi
-
-    # Version
-    if [ "$1" == "-v" ]; then
-        print_version
-        exit $RETURN_CODE_SUCCESS
-    fi
-}
-
-# Find the revision (git commit) argument, either `<rev>` or `<rev>..<rev>` below, as well as the
-# `<file>` argument.
+# Parse the 'git blametool'-specific arguments first. Then, find the revision (git commit) argument,
+# either `<rev>` or `<rev>..<rev>` below, as well as the `<file>` argument.
 # Note: `man git blame` shows these are the possible args:
 #       git blame [-c] [-b] [-l] [--root] [-t] [-f] [-n] [-s] [-e] [-p] [-w] [--incremental]
 #                   [-L <range>] [-S <revs-file>] [-M] [-C] [-C] [-C] [--since=<date>]
 #                   [--progress] [--abbrev=<n>] [<rev> | --contents <file> | --reverse <rev>..<rev>]
 #                   [--] <file>
-get_revision_and_filename() {
+parse_args() {
     # For advanced argument parsing help and demo, see:
     # https://stackoverflow.com/a/14203146/4561887.
+
+    if [ $# -eq 0 ]; then
+        echo "No arguments supplied"
+        print_help
+        exit $RETURN_CODE_ERROR
+    fi
 
     # skip over all optional args beginning with `-`. But, if you see a `-L` or `-S` or
     # `--contents`, skip over the accompanying mandatory positional arg right after those too.
@@ -175,107 +161,89 @@ get_revision_and_filename() {
     # provided) being either `<rev>` or `<rev>..<rev>`.
     POSITIONAL_ARGS_ARRAY=()
     while [[ $# -gt 0 ]]; do
-    arg="$1"
-    # first letter of `arg`; see: https://stackoverflow.com/a/10218528/4561887
-    first_letter="${arg:0:1}"
+        arg="$1"
+        # first letter of `arg`; see: https://stackoverflow.com/a/10218528/4561887
+        first_letter="${arg:0:1}"
 
-    case $first_letter in
-        "-")
-            shift # past argument
-            if [ "$arg" == "-L" ] || [ "$arg" == "-S" ] || [ "$arg" == "--contents" ]; then
-                shift # past value
-            fi
-            ;;
-        *) # unknown option (ie: unmatched in the switch cases above)
-            POSITIONAL_ARGS_ARRAY+=("$1") # save it in an array for later
-            shift # past argument
-            ;;
-    esac
+        case $arg in
+            # Help menu
+            "-h"|"-?")
+                print_help
+                exit $RETURN_CODE_SUCCESS
+                ;;
+            # Version
+            "-v"|"--version")
+                print_version
+                exit $RETURN_CODE_SUCCESS
+                ;;
+            # Unknown option (ie: unmatched in the switch cases above)
+            *)
+                # Only store into the position arguments array those arguments which do NOT
+                # begin with "-"!
+                if [ "$first_letter" != "-" ]; then
+                    POSITIONAL_ARGS_ARRAY+=("$1") # save it in an array for later
+                fi
+
+                shift # past argument
+
+                if [ "$arg" == "-L" ] || [ "$arg" == "-S" ] || [ "$arg" == "--contents" ]; then
+                    shift # past value
+                fi
+                ;;
+        esac
     done
+    # echo "POSITIONAL_ARGS_ARRAY contains: '${POSITIONAL_ARGS_ARRAY[*]}'." # for debugging
 
-    # last filtered arg
-    FILE_IN="${POSITIONAL_ARGS_ARRAY[-1]}"
-    # 2nd-to-last filtered arg
-    REVISION="${POSITIONAL_ARGS_ARRAY[-2]}"
+
+    FILE_IN="" # `<file>`
+    COMMIT=""  # `<rev>` or `<rev>..<rev>`
 
     array_len=${#POSITIONAL_ARGS_ARRAY[@]}
-    if [ $array_len -gt 2 ]; then
-        echo "Warning. 'git blametool' parsing may be faulty. Positional arguments array is longer"
-        echo   "than expected. Please open an issue here:"
-        echo "  https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles/issues"
-        echo "  Include this output. array_len = $array_len. Your cmd:"
-        echo "          $FULL_CMD"
+
+    if [ $array_len -ge 1 ]; then
+        FILE_IN="${POSITIONAL_ARGS_ARRAY[-1]}" # last filtered arg
     fi
 
-    # debugging
-    # TEST 1 INPUT:
-    #       git blametool hey how --are -- -you doing -L l_arg -S s_arg --contents contents_arg -a -b -c
-    # TEST 1 EXPECTED RESULTS:
-    #       last filtered arg        = doing
-    #       2nd-to-last filtered arg = how
-    echo "last filtered arg        = $FILE_IN"
-    echo "2nd-to-last filtered arg = $REVISION"
+    if [ $array_len -ge 2 ]; then
+        COMMIT="${POSITIONAL_ARGS_ARRAY[-2]}" # 2nd-to-last filtered arg
+    fi
 
-    # # testing
-    # OUTSIDE_ARRAY="${POSITIONAL_ARGS_ARRAY[-200]}"
-    # if [ -z  "$OUTSIDE_ARRAY" ]; then
-    #     echo "== is zero-length-string =="
-    # fi
+    if [ $array_len -gt 2 ]; then
+        echo "\
+Warning:
+  This isn't supposed to happen. 'git blametool' parsing may be faulty. Positional arguments
+  array is longer than expected. Please open an issue here:
+  https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles/issues. Include this output.
+  array_len = $array_len. POSITIONAL_ARGS_ARRAY contains: '${POSITIONAL_ARGS_ARRAY[*]}'. Your cmd:
+        $FULL_CMD\
+"
+    fi
 
+    # # debugging
+    # # TEST 1 INPUT:
+    # #       git blametool hey how --are -- -you doing -L l_arg -S s_arg --contents contents_arg -a -b -c
+    # # TEST 1 EXPECTED RESULTS:
+    # #       last filtered arg        = doing
+    # #       2nd-to-last filtered arg = how
+    # echo "last filtered arg        = $FILE_IN"
+    # echo "2nd-to-last filtered arg = $COMMIT"
 
-    # for i in "${!all_args_array[@]}"; do
-    #     arg="${all_args_array[$i]}"
-    #     echo "$arg" # debugging
-    #     echo "end"
-    #     # first_letter="${arg:0:1}"  # See: https://stackoverflow.com/a/10218528/4561887
-    #     # # echo "$first_letter" # debugging
-
-    #     # # skip over all optional args beginning with `-`. But, if you see a `-L` or `-S` or
-    #     # # `--contents`, skip over the accompanying mandatory positional arg right after those too.
-    #     # # This will leave just 1 or 2 positional arguments remaining, with the last arg being
-    #     # # `<file>`, and the 2nd-to-last arg (if it exists at all, since it doesn't have to be
-    #     # # provided) being either `<rev>` or `<rev>..<rev>`.
-    #     # if [ "$arg" == "-L" ] || [ "$arg" == "-S" ] || [ "$arg" == "--contents" ]; then
-
-
-    #     # if [ "$first_letter" != "-" ]; then
-    #     #     positional_args_array+=("$arg") # see: https://stackoverflow.com/a/1951523/4561887
-    #     # fi
-    # done
-
-    # # For debugging
-
-    # # Sample test call: `git blametool hey how --are -- -you doing`
-    # # Expected output:
-    # #       hey
-    # #       how
-    # #       doing
-    # for arg in "${positional_args_array[@]}"; do
-    #     echo "$arg"
-    # done
-
-    # # For debugging
-    # # print the last arg in it
-    # # See: https://unix.stackexchange.com/a/198789/114401
-    # echo "last arg = ${positional_args_array[-1]}"
-    # # print the 2nd to last arg in it
-    # echo "2nd to last arg = ${positional_args_array[-2]}"
-
-    exit
+    get_commit_hash
 }
 
 # Get the short commit hash specified by the user's inputs to `git blame`/`git blametool`.
 # This will be used as part of the temporary file name, for instance, to make it obvious for which
 # commit the temp file is showing `git blame` output.
 get_commit_hash() {
-    # The
-    echo ""
-    # See: https://stackoverflow.com/a/1441062/4561887 and see my comments under that answer too.
-    # git log -1 --pretty=format:"%h" HEAD~..HEAD
+    # For the `--pretty=format` options, see: https://stackoverflow.com/a/1441062/4561887. See my
+    # comments under that answer too.
+    commit_hash="$(git log -1 --pretty=format:"%h" $COMMIT)"
 
-
-    ####### parse and get `HEAD~..HEAD` part.
-    # git rev-parse --short $(git rev-parse HEAD~..HEAD | head -n 1)
+    ret_code="$?"
+    if [ "$ret_code" -ne "$RETURN_CODE_SUCCESS" ]; then
+        echo "Warning: 'git blametool' failed to get git commit hash."
+        commit_hash=""
+    fi
 }
 
 main() {
@@ -284,7 +252,7 @@ main() {
     # get the last argument, which is the file name; see:
     # https://stackoverflow.com/questions/1853946/getting-the-last-argument-passed-to-a-shell-script/1854031#1854031
     FILE_IN="${@: -1}"
-    # echo "$FILENAME" # for debugging
+    echo "$FILE_IN" # for debugging
 
     # Read the user's settings.
     editor="$(git config blametool.editor)"
@@ -339,6 +307,5 @@ main() {
 ALL_ARGS_ARRAY="$@"
 FULL_CMD="$0 $ALL_ARGS_ARRAY"
 
-get_revision_and_filename $ALL_ARGS_ARRAY
 parse_args $ALL_ARGS_ARRAY
 main $ALL_ARGS_ARRAY
