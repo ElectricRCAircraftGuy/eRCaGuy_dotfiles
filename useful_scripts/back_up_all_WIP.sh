8b6d128f24140d4d7d4114aeb36986824c0ca196
Gabriel Staples <ercaguy@gmail.com>
2022-10-02 17:43:10 -0700

    git & Linux cmds doc: add placeholder todo section for more rsync notes
    Also add new placeholder file in useful_scripts/back_up_all_WIP.sh

---
+++ useful_scripts/back_up_all_WIP.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# This file is part of eRCaGuy_dotfiles: https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
+
+# GS
+
+# DESCRIPTION:
+#
+# Back up a bunch of remote files, using rsync.
+
+# INSTALLATION INSTRUCTIONS:
+# TODO
+
+# References:
+# 1. See some of my notes to myself from years ago. I've got some snippets like
+# this to start from.
+
