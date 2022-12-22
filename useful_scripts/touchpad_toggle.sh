f49c2f5f409a1e4b6190894e6d9500698ba5be76
Gabriel Staples <ercaguy@gmail.com>
2022-12-21 18:13:54 -0700

    wip: useful_scripts/touchpad_toggle.sh: begin updating script
    ...to be more user-friendly and to support Ubuntu 22.04, which is very
       different in how you must disable the touchpad!
    
       See my answer: https://askubuntu.com/a/1446479/327339

--- useful_scripts/touchpad_toggle.sh
+++ useful_scripts/touchpad_toggle.sh
@@ -11,13 +11,20 @@
 #   and other devices on and off
 
 # INSTALLATION INSTRUCTIONS:
+# First, familiarize yourself with my instructions here: 
+# [Permanently fix Chrome scroll speed](https://askubuntu.com/a/991680/327339)
+#
 # 1. Ensure ~/bin dir exists:
-#		mkdir -p ~/bin
-# 2. Copy this script to ~/bin with a name that you like. Ex:
-#		cp -i touchpad_toggle.sh ~/bin/gs_touchpad_toggle
-# 3. In the newly-copied file in ~/bin, adjust the USER INPUTS section below, as required. This will
+#           mkdir -p ~/bin
+# 2. Install `imwheel` and symlink over the "~/.imwheelrc" file, following the instructions in 
+#    "eRCaGuy_dotfiles/home/.imwheelrc".
+# 2. Symlink this script to ~/bin with a name that you like. Ex:
+#           cd path/to/here
+#           ln -si "$PWD/touchpad_toggle.sh" ~/bin/gs_touchpad_toggle
+# 3. Adjust the USER INPUTS section below, as required. This will
 #    require running `xinput` once from the command line to ensure that TouchPad/Touchpad is
-#    spelled right, for instance. See below for details.
+#    spelled right, for instance. See and follow the section titled "USER INPUTS--ADJUST THESE!" below for 
+#    details.
 # 4. Create a keyboard shortcut to associate Ctrl + Alt + P with this script via (on Ubuntu 18.04)
 #    Settings --> Devices --> Keyboard --> scroll to very bottom and click the "+" button to add a custom
 #    shortcut, then name it "Touchpad Toggle", give it the command "gs_touchpad_toggle", and associate it
@@ -45,15 +52,15 @@
 #    https://askubuntu.com/questions/844151/enable-disable-touchpad/874865#874865
 # 3. I turn it into a beter script, attach it to a Ctrl + Alt + P shortcut, & do a zenity GUI popup window as well:
 #    https://askubuntu.com/questions/844151/enable-disable-touchpad/1109515#1109515
-# 4. I add imwheel to my script to also fix Chrome mouse scroll wheel speed problem at the same time:
-#    https://askubuntu.com/questions/254367/permanently-fix-chrome-scroll-speed/991680#991680
+# 4. ***** I add imwheel to my script to also fix Chrome mouse scroll wheel speed problem at the same time:
+#    https://askubuntu.com/a/991680/327339
 # 5. I put this script on Github, and posted a snapshot of it on this answer here:
 #    https://askubuntu.com/questions/198572/how-do-i-disable-the-touchscreen-drivers/1206493#1206493
 # 6. I put this script into my eRCaGuy_dotfiles, and continued to improve upon it here:
-#	 https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
+#    https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
 
 # ----------------------------------------------------------------------------------------------------------------------
-# USER INPUTS--ADJUST THESE!:
+# USER INPUTS--ADJUST THESE!: <==========
 
 # `xinput` search strings for these devices
 # - Manually run `xinput` on your PC, look at the output, and adjust these search strings as necessary for your
@@ -61,6 +68,11 @@
 # TOUCHPAD_STR="TouchPad" # some computers need it spelled like this with a capital 'P' in TouchPad
 TOUCHPAD_STR="Touchpad" # other computers need it spelled like this with a lower-case 'p' in Touchpad
 TOUCHSCREEN_STR="Touchscreen"
+
+# Optionally disable toggling the touchpad or touchscreen, or both, if desired, by uncommenting the
+# following lines.
+# TOUCHPAD_STR="NONE"
+# TOUCHSCREEN_STR="NONE"
 # ----------------------------------------------------------------------------------------------------------------------
 
 HELP_STR=\
@@ -71,6 +83,7 @@ HELP_STR=\
 "  --off        = turn touchpad & touchscreen OFF\n"
 
 # Obtain the xinput IDs for the Touchpad & Touchscreen so we can disable/enable them
+if [ "$TOUCHPAD_STR" =  ] ######
 read TouchpadId <<< $( xinput | sed -nre "/${TOUCHPAD_STR}/s/.*id=([0-9]*).*/\1/p" )
 read TouchscreenId <<< $( xinput | sed -nre "/${TOUCHSCREEN_STR}/s/.*id=([0-9]*).*/\1/p" )
 # echo "TouchpadId = $TouchpadId; see output from 'xinput'" # Debug print
@@ -80,15 +93,15 @@ PRINT_TEXT="Touchpad (ID $TouchpadId) &amp; Touchscreen (ID $TouchscreenId) "
 
 newstate=
 if [ "$#" -eq "0" ];  then
-	# No positional parameters, so determine the actual current state so we can toggle it
-	state=$( xinput list-props "$TouchpadId" | grep "Device Enabled" | grep -o "[01]$" )
-	if [ "$state" -eq '1' ];then
-		# state is ON, so toggle it to OFF
-		newstate="OFF"
-	else
-		# state is OFF, so toggle it to ON
-		newstate="ON"
-	fi
+    # No positional parameters, so determine the actual current state so we can toggle it
+    state=$( xinput list-props "$TouchpadId" | grep "Device Enabled" | grep -o "[01]$" )
+    if [ "$state" -eq '1' ];then
+        # state is ON, so toggle it to OFF
+        newstate="OFF"
+    else
+        # state is OFF, so toggle it to ON
+        newstate="ON"
+    fi
 elif [ "$#" -eq "1" ] && [ "$1" = "--on" ];  then
     newstate="ON"
 elif [ "$#" -eq "1" ] && [ "$1" = "--off" ];  then
@@ -97,29 +110,29 @@ elif [ "$#" -eq "1" ] && ([ "$1" = "--help" ] || [ "$1" = "-h" ]);  then
     echo -e "$HELP_STR"
     exit
 else
-	echo "ERROR: Invalid input parameters."
-	echo -e "$HELP_STR"
-	exit
+    echo "ERROR: Invalid input parameters."
+    echo -e "$HELP_STR"
+    exit
 fi
 
 if [ "$newstate" = "OFF" ];then
-	# Turn touchpad & touchscreen OFF, and turn imwheel ON to improve mouse wheel scroll speed since user must be using
-	# an external mouse
-	echo "Turning TouchpadId $TouchpadId & TouchscreenId $TouchscreenId OFF."
+    # Turn touchpad & touchscreen OFF, and turn imwheel ON to improve mouse wheel scroll speed since user must be using
+    # an external mouse
+    echo "Turning TouchpadId $TouchpadId & TouchscreenId $TouchscreenId OFF."
     imwheel -b "4 5" # turn on imwheel to help external mouse wheel scroll speed be better
     xinput --disable "$TouchpadId"
     xinput --disable "$TouchscreenId"
     zenity --info --text "${PRINT_TEXT} DISABLED" --timeout=2
 elif [ "$newstate" = "ON" ];then
-	# Turn touchpad & touchscreen ON, & turn imwheel OFF so it doesn't interfere w/trackpad scrolling, since user must
-	# be using the touchpad and/or touchscreen
-	echo "Turning TouchpadId $TouchpadId & TouchscreenId $TouchscreenId ON."
+    # Turn touchpad & touchscreen ON, & turn imwheel OFF so it doesn't interfere w/trackpad scrolling, since user must
+    # be using the touchpad and/or touchscreen
+    echo "Turning TouchpadId $TouchpadId & TouchscreenId $TouchscreenId ON."
     killall imwheel # turn OFF imwheel to keep imwheel from interfering with proper track pad scrolling
     xinput --enable "$TouchpadId"
     xinput --enable "$TouchscreenId"
     zenity --info --text "${PRINT_TEXT} ENABLED" --timeout=2
 else
-	echo "ERROR: Invalid newstate value of \"$newstate\"; must use only \"OFF\" or \"ON\"."
+    echo "ERROR: Invalid newstate value of \"$newstate\"; must use only \"OFF\" or \"ON\"."
 fi
 
 
