87a6605c4bf780f08e8da26edb4826a8e83c0e65
Gabriel Staples <ercaguy@gmail.com>
2024-01-26 13:41:29 -0700

    useful_scripts/git-difft.sh: release v0.2.0:
    Allow specifying a commit range, and/or files and directories to track.

--- useful_scripts/git-difft.sh
+++ useful_scripts/git-difft.sh
@@ -36,13 +36,16 @@
 # 1. https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world/blob/master/bash/argument_parsing__3_advanced__gen_prog_template.sh
 #    - a very advanced, general Bash program example.
 # 1. GitHub Copilot helped me with the algorithm in `main()`.
-# 1.
+# 1. "eRCaGuy_hello_world/bash/argument_parsing__3_advanced__gen_prog_template.sh" - for help 
+#    parsing arguments. See:
+#    https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world/blob/master/bash/argument_parsing__3_advanced__gen_prog_template.sh
+# 1. 
 #
 
 RETURN_CODE_SUCCESS=0
 RETURN_CODE_ERROR=1
 
-VERSION="0.1.0"
+VERSION="0.2.0"
 AUTHOR="Gabriel Staples"
 
 SCRIPT_NAME="$(basename "$0")"
@@ -59,16 +62,29 @@ $VERSION_SHORT_STR
 Iterate through all commits going backwards from HEAD to the first commit, one commit at a time,
 running 'git difftool' on each one to see the changes it introduced.
 
+- Press Ctrl + C once, then Enter, to break out of the current 'git difftool' command, but continue 
+on with the previous commit. 
+- Press Ctrl + C twice to exit out of the whole program.
+
 USAGE:
-    $SCRIPT_NAME [OPTIONS]
+    $SCRIPT_NAME [OPTIONS] [[commit] [commit_start~..commit_end]] -- [file1 file2 file3 ...]
 
 OPTIONS
     -h, -?
         Print help menu
+    -v, --version
+        Print version information.
+    --
+        Lists of files or directories go after this point.
 
 EXAMPLE USAGES:
     $SCRIPT_NAME -h
         Print help menu.
+    $SCRIPT_NAME
+        Start running 'git difftool' on the commit starting at HEAD (the currently-checked-out
+        commit).
+    $SCRIPT_NAME HEAD
+        Same as above.
     $SCRIPT_NAME HEAD~
         Start running 'git difftool' on the commit starting at HEAD~ (one before HEAD).
     $SCRIPT_NAME HEAD~2
@@ -77,6 +93,20 @@ EXAMPLE USAGES:
         Start running 'git difftool' on commit hash abcdefg.
     $SCRIPT_NAME my_branch
         Start running 'git difftool' on the commit at the tip of branch 'my_branch'.
+    $SCRIPT_NAME commit1~..commit2
+        Run 'git difftool' on all commits between commit1 and commit2, inclusive.    
+    $SCRIPT_NAME commit1..commit2
+        Run 'git difftool' on all commits between commit1 and commit2, including commit2 but
+        NOT including commit1.
+    $SCRIPT_NAME commit1~..commit2 -- file1 file2 file3
+        Run 'git difftool' on all commits between commit1 and commit2, inclusive, but only
+        for the files file1, file2, and file3.
+    $SCRIPT_NAME -- path/to/file1
+        Start running 'git difftool' on the commit starting at HEAD, but only for the file
+        \"path/to/file1\".
+    $SCRIPT_NAME -- path/to/dir1
+        Start running 'git difftool' on the commit starting at HEAD, but only for the files
+        in the directory \"path/to/dir1/\".
 
 This program is part of eRCaGuy_dotfiles: https://github.com/ElectricRCAircraftGuy/eRCaGuy_dotfiles
 by Gabriel Staples.
@@ -84,6 +114,10 @@ by Gabriel Staples.
 
 print_help() {
     echo "$HELP_STR" | less -RFX
+}
+
+print_version() {
+    echo "$VERSION_LONG_STR"
 }
 
 # Print all arguments, for debugging.
@@ -99,14 +133,53 @@ debugging_print_all_args() {
 }
 
 parse_args() {
-    # debugging_print_all_args "$@"  # debugging
+    # See my code here for advanced argument parsing:
+    # https://github.com/ElectricRCAircraftGuy/eRCaGuy_hello_world/blob/master/bash/argument_parsing__3_advanced__gen_prog_template.sh
+    ALL_ARGS_ARRAY=("$@")  # See: https://stackoverflow.com/a/70572787/4561887
+    # echo "ALL_ARGS_ARRAY = ${ALL_ARGS_ARRAY[@]}"  # debugging
+    POSITIONAL_ARGS_ARRAY=()
+    filenames_array=()
+    while [ "$#" -gt 0 ]; do
+        arg="$1"
+        # get first letter of `arg`; see: https://stackoverflow.com/a/10218528/4561887
+        # This is a form of bash "array slicing", treating the string like an array of chars,
+        # so see also my answer about array slicing here:
+        # https://unix.stackexchange.com/a/664956/114401
+        first_letter="${arg:0:1}"
 
-    if [ "$1" = "-h" ] || [ "$1" = "-?" ]; then
-        print_help
-        exit $RETURN_CODE_SUCCESS
-    fi
+        case $arg in
+            # Help menu
+            "-h"|"-?"|"--help")
+                print_help
+                exit $RETURN_CODE_SUCCESS
+                ;;
+            # Version
+            "-v"|"--version")
+                print_version
+                exit $RETURN_CODE_SUCCESS
+                ;;
+            # Filename args
+            "--")
+                # echo "Found '--' arg!"  # debugging
+                # Collect *all* remaining args into the `filenames_array`
+                shift # past argument
+                all_remaining_args=("$@")
+                filenames_array=("${all_remaining_args[@]}")
+                break
+                ;;
+            # All positional args (ie: unmatched in the switch cases above)
+            *)
+                # error out for any unexpected options passed in
+                if [ "$first_letter" = "-" ]; then
+                    echo_error "Invalid optional argument ('$1'). See help menu for valid options."
+                    exit $RETURN_CODE_ERROR
+                fi
 
-    STARTING_COMMIT="$1"
+                POSITIONAL_ARGS_ARRAY+=("$1")  # save positional arg into array
+                shift # past argument (`$1`)
+                ;;
+        esac
+    done
 }
 
 handle_ctrl_c() {
@@ -114,7 +187,7 @@ handle_ctrl_c() {
     trap - SIGINT
 
     echo -e "\nCtrl + C captured! Exit out of the whole program? (y/N)"
-    echo    "[Press n or Enter to continue on with the next commit]"
+    echo    "[Press n or Enter to continue on with the previous commit]"
     read -r -n 1 answer
     echo ""
 
@@ -128,14 +201,23 @@ handle_ctrl_c() {
 
 main() {
     # Get a list of all commit hashes
-    commits=$(git log --pretty=format:"%h" $STARTING_COMMIT)
-    if [ $? -ne 0 ]; then
+    commits=$(git log --pretty=format:"%h" "${ALL_ARGS_ARRAY[@]}")
+    if [ "$?" -ne "0" ]; then
         echo "ERROR: 'git log' command failed!"
         debugging_print_all_args "$@"
         echo ""
-        print_help
+        echo "Run '$SCRIPT_NAME -h' for help."
         exit $RETURN_CODE_ERROR
     fi
+
+    # debugging
+    # echo -e "commits = \n$commits"
+
+    echo "Analyzing previous commits in 'git difftool', one commit at a time."
+    echo "- Press Ctrl + C, then Enter, to skip the current commit and continue on with the previous"\
+         "commit."
+    echo "- Press Ctrl + C twice to exit out of the whole program."
+    echo ""
 
     # Loop through the list of commit hashes
     i=0
@@ -152,19 +234,22 @@ main() {
         git log -1 $commit
 
         echo -e "\n----------------------------------------"
-        printf "%s\n" "Running 'git difftool $commit~..$commit'. Run this to pick back up here:" \
-            "$SCRIPT_NAME $commit"
+        echo "Running 'git difftool $commit~..$commit -- ${filenames_array[@]}'."
+        echo "Run this to pick back up here:"
+        echo "    $SCRIPT_NAME $commit -- ${filenames_array[@]}"
         echo      "----------------------------------------"
 
         # Capture Ctrl + C so we can break out of `git difftool`, but NOT necessarily out of the
         # whole program at this time!
         trap 'handle_ctrl_c' SIGINT
 
-        git difftool $commit~..$commit
+        git difftool $commit~..$commit -- "${filenames_array[@]}"
 
         echo ""
         ((i++))
     done
+
+    echo "Done with all commits! Exiting '$SCRIPT_NAME'."
 }
 
 # Determine if the script is being sourced or executed (run).
